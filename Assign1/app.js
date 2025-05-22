65e52fc46b61fa83a40818bfee518ce73bda0430
Amber-Aly <amberaly123@gmail.com>
2025-05-22 14:29:36 +0500

    Add files via upload

---
+++ Assign1/app.js
@@ -0,0 +1,7 @@
+alert("Welcome to my page");
+
+alert ("Javascript Alert\nPlease enter the valid password");
+
+alert ("Welcome to JS land....\nHappy Coding!");
+
+alert ("Hello...I can run JS through my web browser's console.")
